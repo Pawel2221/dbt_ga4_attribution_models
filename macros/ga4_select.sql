0319c6f3f4c5f8a62b2c321d0d8096eb4373e88f
Pawel2221 <pawel.olejarczyk1@gmail.com>
2024-09-01 19:20:56 +0200

    order event name change

--- macros/ga4_select.sql
+++ macros/ga4_select.sql
@@ -6,7 +6,7 @@
   user_pseudo_id,
   COALESCE(CONCAT(user_pseudo_id, {{ ga4_unnest_key('event_params', 'ga_session_id', 'int_value') }}), user_pseudo_id) AS session_id,
   CASE
-      WHEN event_name = 'sign_up_completed' THEN event_timestamp
+      WHEN event_name = 'purchase' THEN event_timestamp
       ELSE NULL
   END AS is_with_order,
   {{ ga4_unnest_key('event_params', 'page_location', 'string_value') }} AS page_location,
